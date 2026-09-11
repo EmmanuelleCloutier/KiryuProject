d72dba157b6d7aa964080fec4d9b64399670e1d4
Strawbeewwy <Strawbeewwy@gmail.com>
2026-09-11 16:07:44 -0400

    Implement core projectile system
    Establishes `Projectile`, `ProjectileType`, and `ProjectileRenderer` classes to provide a foundational structure for managing different types of in-game projectiles and their properties.

--- src/Game/code/Projectile.java
+++ src/Game/code/Projectile.java
@@ -1,4 +1,45 @@
 package Game.code;
 
+import KiryuEngine.KiryuPhysics.Vector3;
+import KiryuEngine.KiryuRendering.ProjectileRenderer;
+
 public class Projectile {
+
+  private Vector3 position;
+  private final double velocity;
+  private final double mass;
+  private final ProjectileRenderer renderer;
+  private final ProjectileType type;
+
+  public Projectile(ProjectileType type, Vector3 position){
+    this.position = position;
+    this.type = type;
+    this.velocity = type.getVelocity();
+    this.mass = type.getMass();
+    this.renderer = type.getRenderer();
+  }
+
+  public Vector3 getPosition() {
+    return position;
+  }
+
+  public void setPosition(Vector3 position) {
+    this.position = position;
+  }
+
+  public double getVelocity() {
+    return velocity;
+  }
+
+  public ProjectileRenderer getRenderer() {
+    return renderer;
+  }
+
+  public ProjectileType getType() {
+    return type;
+  }
+
+  public double getMass() {
+    return mass;
+  }
 }
--- src/Game/code/ProjectileType.java
+++ src/Game/code/ProjectileType.java
@@ -1,4 +1,52 @@
 package Game.code;
 
+
+import KiryuEngine.KiryuRendering.ProjectileRenderer;
+
+import static Game.code.ProjectileType.Defaults.*;
+
 public enum ProjectileType {
+  Ball(DEFAULT_BALL_VELOCITY,DEFAULT_BALL_MASS, new ProjectileRenderer()),
+  CannonBall(DEFAULT_CANNONBALL_VELOCITY,DEFAULT_CANNONBALL_MASS, new ProjectileRenderer()),
+  Laser(DEFAULT_LASER_VELOCITY,DEFAULT_LASER_MASS, new ProjectileRenderer()),
+  FireBall(DEFAULT_FIREBALL_VELOCITY,DEFAULT_FIREBALL_MASS, new ProjectileRenderer());
+
+  private final double velocity;
+  private final double mass;
+  private final ProjectileRenderer renderer;
+
+  ProjectileType(double velocity, double mass, ProjectileRenderer renderer){
+    this.velocity = velocity;
+    this.mass = mass;
+    this.renderer = renderer;
+  }
+
+  public double getVelocity() {
+    return velocity;
+  }
+
+  public double getMass() {
+    return mass;
+  }
+
+  public ProjectileRenderer getRenderer() {
+    return renderer;
+  }
+
+
+  static class Defaults {
+
+    public static final double DEFAULT_BALL_VELOCITY = 10.f;
+    public static final double DEFAULT_BALL_MASS = 2.f;
+
+    public static final double DEFAULT_CANNONBALL_VELOCITY = 10.f;
+    public static final double DEFAULT_CANNONBALL_MASS = 10.f;
+
+    public static final double DEFAULT_LASER_VELOCITY = 50.f;
+    public static final double DEFAULT_LASER_MASS = 0.f;
+
+    public static final double DEFAULT_FIREBALL_VELOCITY = 10.f;
+    public static final double DEFAULT_FIREBALL_MASS = 5.f;
+  }
+
 }
