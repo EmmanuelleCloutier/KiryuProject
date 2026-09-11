6c9488f704a5f2fdf3e0c3febb863c9e237abbd8
Strawbeewwy <Strawbeewwy@gmail.com>
2026-09-11 16:50:11 -0400

    movement for Projectile
    Refactors the `Projectile` class to manage its own 3D position updates. Introduces a `direction` vector and a scalar `speed` to define movement.
    
    Adds an `updateMovement` method responsible for calculating and applying the projectile's displacement over a given `deltaTime`. This establishes the core logic for dynamic in-game projectile trajectories.

--- src/Game/code/ProjectileType.java
+++ src/Game/code/ProjectileType.java
@@ -15,36 +15,70 @@ public enum ProjectileType {
   private final double mass;
   private final ProjectileRenderer renderer;
 
+  /**
+   * all of these parameters are determined by the default subclass of this enum
+   * please refer to that when you want to tweak some values
+   * @param velocity the speed at which the type should travel
+   * @param mass the mass the type should have
+   * @param renderer the renderer used by this type
+   */
   ProjectileType(double velocity, double mass, ProjectileRenderer renderer){
     this.velocity = velocity;
     this.mass = mass;
     this.renderer = renderer;
   }
 
+  /**
+   *
+   * @return this type's default velocity
+   */
   public double getVelocity() {
     return velocity;
   }
 
+  /**
+   *
+   * @return this type's default mass
+   */
   public double getMass() {
     return mass;
   }
 
+  /**
+   *
+   * @return this type's default renderer
+   */
   public ProjectileRenderer getRenderer() {
     return renderer;
   }
 
-
+  /**
+   * this class contains all the default setting for every type of
+   * projectiles.
+   */
   static class Defaults {
 
+    /**
+     * BALL DEFAULTS
+     */
     public static final double DEFAULT_BALL_VELOCITY = 10.f;
     public static final double DEFAULT_BALL_MASS = 2.f;
 
+    /**
+     * CANNON BALL DEFAULTS
+     */
     public static final double DEFAULT_CANNONBALL_VELOCITY = 10.f;
     public static final double DEFAULT_CANNONBALL_MASS = 10.f;
 
+    /**
+     * LASER DEFAULTS
+     */
     public static final double DEFAULT_LASER_VELOCITY = 50.f;
     public static final double DEFAULT_LASER_MASS = 0.f;
 
+    /**
+     * FIREBALL DEFAULTS
+     */
     public static final double DEFAULT_FIREBALL_VELOCITY = 10.f;
     public static final double DEFAULT_FIREBALL_MASS = 5.f;
   }
