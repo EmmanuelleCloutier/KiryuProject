23aa6eadcf71de78f3c91b55beb11599634c95fa
Strawbeewwy <Strawbeewwy@gmail.com>
2026-09-12 01:02:51 -0400

    Add Sprite class for visual rendering
    This class provides a basic implementation for loading and drawing 2D image assets, conforming to the `Renderable` interface.

--- src/KiryuEngine/KiryuRendering/Sprite.java
+++ src/KiryuEngine/KiryuRendering/Sprite.java
@@ -1,2 +1,33 @@
-package KiryuEngine.KiryuRendering;public class Sprite {
+package KiryuEngine.KiryuRendering;
+
+import KiryuEngine.KiryuPhysics.Vector3;
+import processing.core.PApplet;
+import processing.core.PImage;
+import static processing.awt.ShimAWT.loadImage;
+import static processing.core.PConstants.CENTER;
+
+public class Sprite implements Renderable{
+
+  private final String spritePath;
+  private PImage sprite = null;
+
+  public Sprite(String spritePath){
+    this.spritePath = spritePath;
+  }
+
+  public String getSpritePath() {
+    return spritePath;
+  }
+
+  @Override
+  public void draw(PApplet sketch,Vector3 position) {
+    sketch.imageMode(CENTER);
+    sketch.image(sprite,(float)position.x,(float)position.x);
+  }
+
+
+  @Override
+  public void loadRenderableImage(PApplet sketch) {
+    this.sprite = loadImage(sketch, this.spritePath);
+  }
 }
